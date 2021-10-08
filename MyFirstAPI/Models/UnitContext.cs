657e2b9e9caa0b6325792a009e35a7b4d1668dda
Benson Lin <blin0008@student.monash.edu>
2021-10-08 23:38:57 +1100

    Add project files.

---
+++ MyFirstAPI/Models/UnitContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstAPI.Models
+{
+    public class UnitContext : DbContext
+    {
+        public UnitContext(DbContextOptions<UnitContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Unit> Units { get; set; }
+    }
+}
